b5d86fea0f7d667e3581b46809ba7cfd5d45cc5e
Abdulmuhsin Ababtain <abdumuhsinsma@gmail.com>
2024-04-28 14:28:44 +0300

    Created classes and interfaces

---
+++ src/User/Interfaces/ControllerTemplate.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.User.Controllers
+{
+    public class ControllerTemplate : ControllerBase
+    {
+        
+    }
+}
