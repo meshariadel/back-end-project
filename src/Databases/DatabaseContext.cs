76734939c81bf99f823289290595926a22889375
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-29 14:13:32 +0300

    UserOrder Merege with Product

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -7,6 +7,7 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src;
 
 public class DatabaseContext
 {
+    public List<UserOrder> userOrder;
     public List<Product> products;
     public DatabaseContext()
     {
@@ -14,8 +15,12 @@ public class DatabaseContext
             new Product( ProductSize.L, "Red",  155.99 ,12 ,"picture","T-shirt","red t-shirt"),
             new Product( ProductSize.S, "Red",  155.99 ,12 ,"picture","T-shirt","red t-shirt"),
             new Product( ProductSize.M, "Red",  155.99 ,12 ,"picture","T-shirt","red t-shirt")
+            ];
+            userOrder = [
+                new UserOrder("1","1","1","Riyadh",DateTime.Now,new DateTime(2024,05,01),UserOrder.OrderStatus.OutForDelivery)
+            ];
 
 
-        ];
+        
     }
 }
