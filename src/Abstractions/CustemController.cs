19f1789babeecc4ab22b41c265878457c66337c0
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-29 06:23:12 +0300

    Create new Folder Abstractions and two interface class IUserOrderRepository & IUserOrderService and moving CustemController on it

--- src/Abstractions/CustemController.cs
+++ src/Abstractions/CustemController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Controllers
+namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Abstractions
 {
     [ApiController]
     [Route("api/v1/[controller]")]
