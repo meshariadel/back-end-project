956fcf0aab714fbb27bd0a12505f722c61005672
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-29 13:54:35 +0300

    update program.cd

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,7 @@
+using sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Abstractions;
+using sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Repository;
+using sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IUserOrderService, UserOrderService>();
+builder.Services.AddScoped<IUserOrderRepository, UserOrderRepository>();
 
 var app = builder.Build();
 
--- src/Repository/UserOrderRepository.cs
+++ src/Repository/UserOrderRepository.cs
@@ -20,5 +20,8 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Re
         {
             return _userOrder;
         }
+         
+        
+
     }
 }
