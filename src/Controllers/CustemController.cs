40057fbdf3869f25326386cd9fda09dfc331d4f7
iplmr <45397969+iplmr@users.noreply.github.com>
2024-04-29 06:02:29 +0300

    Create new Folder Controllers and Class CustemController

---
+++ src/Controllers/CustemController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public abstract class CustemController : ControllerBase
+    {
+        
+    }
+}
