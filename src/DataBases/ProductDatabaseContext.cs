5feb0282cd6511f28ace15f40ea26618706586ac
MJZ-7 <mo0ody5599@hotamil.com>
2024-05-02 10:38:42 +0300

    configuring db

--- src/DataBases/ProductDatabaseContext.cs
+++ src/DataBases/ProductDatabaseContext.cs
@@ -1,18 +1,22 @@
+using Microsoft.EntityFrameworkCore;
 using sda_onsite_2_csharp_backend_teamwork_The_countryside_developers.src.Entities;
 namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
 {
-    public class ProductDatabaseContext
+    public class DatabaseContext : DbContext
     {
-        public IEnumerable<Product> products;
-        public ProductDatabaseContext()
+        public DbSet<Product> Products { get; set; }
+        private IConfiguration _config;
+        public DatabaseContext(IConfiguration config)
         {
-            products = [
-                new Product("0", ProductSize.S, "Red", 159.99 ,14 ,"picture","T-shirt","red t-shirt"),
-            new Product("1", ProductSize.L, "Red",  155.99 ,12 ,"picture","Coat","red t-shirt"),
-            new Product( "2",ProductSize.S, "Red",  155.99 ,12 ,"picture","Shirt","red t-shirt"),
-            new Product( "3",ProductSize.M, "blue",  159.99 ,12 ,"picture","Jeans","blue t-shirt")
-                ];
+            _config = config;
 
         }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql(@"Host: localhost; Username=myLogin; Password=mM121233; Database= ecommerce");
+
+
     }
 }
--- src/Mappers/Mapper.cs
+++ src/Mappers/Mapper.cs
@@ -1,20 +1,23 @@
 using AutoMapper;
-
 namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
 {
-    public class Mapper :Profile
+    public class Mapper : Profile
     {
-        
-            public Mapper(){
-            CreateMap<User,UserReadDto>();
-            CreateMap<UserReadDto,User>();
-            CreateMap<UserReadDto,UserUpdateDto>();
-            CreateMap<UserUpdateDto,UserReadDto>();
-            CreateMap<User,UserUpdateDto>();
-            CreateMap<UserUpdateDto,User>();
+
+        public Mapper()
+        {
+            //User mapper
+            CreateMap<User, UserReadDto>();
+            CreateMap<UserReadDto, User>();
+            CreateMap<UserReadDto, UserUpdateDto>();
+            CreateMap<UserUpdateDto, UserReadDto>();
+            CreateMap<User, UserUpdateDto>();
+            CreateMap<UserUpdateDto, User>();
+
+            //Product mapper
             CreateMap<Product, ProductReadDto>();
             CreateMap<ProductReadDto, Product>();
 
         }
     }
-    }
+}
