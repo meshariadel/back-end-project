3a93e2386151233c85ebb765a639547cf78c2695
MJZ-7 <mo0ody5599@hotamil.com>
2024-05-06 16:15:26 +0300

    deleteOne for product

--- src/Abstractions/IProductService.cs
+++ src/Abstractions/IProductService.cs
@@ -3,8 +3,9 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
     public interface IProductService
     {
         public IEnumerable<ProductReadDto> FindAll();
-        public ProductReadDto? FindOne(string product);
+        public ProductReadDto? FindOne(Guid product);
         public Product CreateOne(Product product);
-        public Product UpdateOne(string productName, Product product);
+        public Product UpdateOne(Guid productId, Product product);
+        public bool DeleteOne(Guid id);
     }
 }
