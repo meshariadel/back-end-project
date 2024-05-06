9df1bbe9c616cd770d26ce1211827b0e69dfcd3a
Abdulmuhsin Ababtain <abdumuhsinsma@gmail.com>
2024-05-06 16:04:28 +0300

    Linked and tested user entity to the DB

--- src/User/Entities/User.cs
+++ src/User/Entities/User.cs
@@ -4,23 +4,12 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
     {
 
 
-        public string UserId { get; set; }
+        public Guid UserId { get; set; }
         public string FullName { get; set; }
-        public string Role { get; set; }
+        public string? Role { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public User(string userId, string fullName, string role, string email, string password)
-        {
-
-            UserId = userId;
-            FullName = fullName;
-            Role = role;
-            Email = email;
-            Password = password;
 
 
-        }
-
-        public User(){}
     }
 }
