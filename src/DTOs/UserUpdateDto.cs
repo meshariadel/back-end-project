f022332ad80cd17982da278f2e4edd5512b8ac5f
Abdulmuhsin Ababtain <abdumuhsinsma@gmail.com>
2024-05-07 20:40:24 +0300

    Fixed some issues with User DTOs

--- src/DTOs/UserUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
-{
-    public class UserUpdateDto
-    {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
--- src/User/Entities/User.cs
+++ src/User/Entities/User.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
 {
     public class User
@@ -5,8 +7,10 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
 
 
         public Guid UserId { get; set; }
+        [MaxLength(50)]
         public string FullName { get; set; }
         public string? Role { get; set; }
+        [EmailAddress, Required]
         public string Email { get; set; }
         public string Password { get; set; }
 
